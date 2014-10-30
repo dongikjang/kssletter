8c9cd3fe972aa7c486047ba31f079b1beec2289d
Dongik Jang <dongik.s.jang@gmail.com>
2014-10-30 18:33:14 +0900

    Update seoul_subway2.R

--- seoul_subway2.R
+++ seoul_subway2.R
@@ -39,7 +39,7 @@ lines(nc2, col=alpha("#C82127",1), lwd=1)
 colss <- alpha(c("#00498B", "#009246", "#F36630", "#00A2D1", "#A064A3", "#9E4510", "#5D6519", "#D6406A", "#AA9872"), 1)
 for(i in 1:9){
   nc2_file <- paste(pathss, "지하철_서울", i, "호선2.shp", sep="")
-  nc2 <- readShapeLines(nc2_file, proj4string=CRS("+proj=tmerc +lat_0=38 +lon_0=128 +k=0.9999 +x_0=400000 +y_0=600000 +ellps=bessel +units=m +no_defs +towgs84=-115.80,474.99,674.11,1.16,-2.31,-1.63,6.43"))
+  nc2 <- readShapeLines(nc2_file, proj4string=CRS("+proj=tmerc +lat_0=38 +lon_0=128 +k=0.9999 +x_0=400000 +y_0=600000 +ellps=bessel +units=m +no_defs"))
   nc22 <- spTransform(nc2, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +units=m +no_defs"))
   lines(nc22, col=colss[i], lwd=1)
 }
